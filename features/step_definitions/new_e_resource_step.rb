aab96cd6c7f4dd1fbaf9f9c462193bc31406d180
hemalathas <hemalathas@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-08-26 07:34:21 +0000

    Added script for create new E-resource and link PO document to E-resource document

---
+++ features/step_definitions/new_e_resource_step.rb
@@ -0,0 +1,23 @@
+Given(/^I logged in with ole-quickstart$/) do
+  log_in("ole-quickstart")
+end
+
+When(/^I give material,format and content type and save the document$/) do
+  @eresouce = make EResourceDocument , :tab => 'green'
+  @eresouce.create_e_resource_document
+end
+
+Then(/^the EResource document should be saved with the status saved$/) do
+  visit E_resource do |page|
+    page.doc_search
+    sleep(3)
+    page.doc_id.set $document_id
+    page.search_req
+    page.doc_id_link
+    page.windows[1].use
+    sleep(10)
+    saved = page.doc_status
+    puts saved
+    page.doc_status.should == "SAVED"
+  end
+end
---
+++ lib/ole/data_objects/e_resource_document.rb
@@ -0,0 +1,99 @@
+class EResourceDocument < DataFactory
+  include Foundry
+  include DateFactory
+  include StringFactory
+  include Workflows
+
+  attr_accessor :title,
+                :material_type,
+                :format_type,
+                :content_type,
+                :local_id,
+                :e_resource_identifier,
+                :po_identifier,
+                :tab
+
+  def initialize(browser, opts={})
+    @browser = browser
+    defaults = {
+        title: "test",
+        material_type: ["Journal" , "Database"].sample,
+        format_type: ["E-Book" , "E-Journal" , "Book" , "Journal" , "DVD" , "CD" ,"Other"].sample,
+        content_type: ["Text" , "Images"].sample
+    }
+    set_options(defaults.merge(opts))
+
+  end
+
+  def requisition_local_id
+    on E_resource do |page|
+      page.doc_search
+      sleep(5)
+      page.doc_id.set $doc_value
+      page.search_req
+      sleep(3)
+      page.doc_id_link
+      page.windows[1].use
+      page.click_title
+      page.windows[2].use
+      page.click_holdings
+      page.windows[3].use
+      @local_id = page.holding_local_id
+      puts @local_id
+      page.windows[3].close
+      page.windows[2].close
+      page.windows[1].use
+      page.view_related_documents
+      @po_identifier = page.po_id
+      puts "po number is #@po_identifier"
+      page.windows[1].close
+    end
+  end
+
+  def create_e_resource_document
+    visit E_resource do |page|
+      if(@tab == "green")
+      page.select_acquire
+      else
+       page.select_acquire_red
+      end
+      page.e_resource
+      page.doc_name.set @title
+      page.material_type.select (@material_type)
+      page.format_type.select (@format_type)
+      page.content_type.select (@content_type)
+      $document_id = page.doc_value
+      page.save
+      @e_resource_identifier = page.e_resource_id
+      sleep(5)
+    end
+  end
+
+  def search_e_resource
+    visit E_resource do |page|
+      page.e_search
+      page.record_id.select ("E-Resource Record Id")
+      page.criteria.set @e_resource_identifier
+      page.search_criteria
+      page.open_e_res
+      page.windows[1].use
+      page.open_e_holding
+      page.create_instance
+      page.select_instance_id
+      page.windows[2].use
+      page.search_conditions.select ("Local Identifier")
+      page.doc_type.select ("Holdings")
+      page.search_text.set @local_id
+      page.search_button
+      page.search_result
+      page.holdings
+      page.holdings_search_results
+      page.holdings_link
+      sleep(3)
+      page.windows[1].use
+      page.save_instance
+      sleep(3)
+
+    end
+  end
+end
