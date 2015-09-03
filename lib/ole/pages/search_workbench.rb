0e7bf76447c37b06ae2db19b952291bb0cd42c71
hemalathas <hemalathas@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-09-03 12:21:54 +0000

    Modified search_workbench page for marc editor

--- lib/ole/pages/search_workbench.rb
+++ lib/ole/pages/search_workbench.rb
@@ -12,4 +12,10 @@ class Search_workbench < PageFactory
   element(:datafield_tag) { |b| b.form(id:"kualiForm").text_field(id:"dataField_tag_id_line1_control")}
   element(:datafield_value) { |b| b.form(id:"kualiForm").text_field(id:"dataField_value_id_line1_control")}
   action(:submit) { |b| b.form(id:"kualiForm").button(id:"submitEditor").click}
+  element(:search_selection_type) { |b| b.iframe(id:"iframeportlet").select(id:"DocumentAndSearchSelectionType_DocType_control")}
+  element(:item_search_condition) { |b| b.iframe(id:"iframeportlet").select(id:"item_SearchConditions_DocField_id_line0_control")}
+  element(:set_item_barcode) { |b| b.iframe(id:"iframeportlet").text_field(id:"SearchConditions_SearchText_id_line0_control")}
+  action(:data_field) { |b| b.iframe(id:"iframeportlet").table(class:"table table-condensed table-bordered uif-tableCollectionLayout dataTable").a(target:"_blank").when_present(60).click}
+  value(:search_local_id) { |b| b.form(id:"kualiForm").span(id:"LocalItem_control").text}
+
 end
