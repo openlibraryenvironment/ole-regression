d94602f1f7ae1545c23c19ef6d94a39d3a92a2bd
peris <peris@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-09-09 12:57:27 +0000

    Upgraded firefox to 38;

--- upgrade_firefox.sh
+++ upgrade_firefox.sh
@@ -9,7 +9,7 @@ unlink /usr/bin/firefox
 rm -rf /usr/lib/firefox
 
 cd $workpath
-wget -r --no-parent --reject "index.html*" -nH --cut-dirs=7 https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/27.0/linux-x86_64/en-US/
+wget -r --no-parent --reject "index.html*" -nH --cut-dirs=7 https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/38.0/linux-x86_64/en-US/
 tar xvf firefox-27.0.tar.bz2
 cd bin
 ln -s ../firefox/firefox
