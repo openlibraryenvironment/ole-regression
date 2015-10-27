e8c827a1b9bf5ad688f4b927975b6986f6c95075
hemalathas <hemalathas@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2015-10-27 11:29:53 +0000

    Added script for global edit in describe module

---
+++ features/describe/global_edit.feature
@@ -0,0 +1,6 @@
+@nightly
+  Feature: Global edit of items/holdings
+    Scenario: Edit multiple items or holdings at a time
+      Given I am logged in as ole-quickstart for global edit
+      When I create multiple item and edit those items
+      Then the changes should be successfully updated
---
+++ features/step_definitions/global_edit_step.rb
@@ -0,0 +1,27 @@
+Given(/^I am logged in as ole-quickstart for global edit$/) do
+  log_in("ole-quickstart")
+end
+
+When(/^I create multiple item and edit those items$/) do
+  @marc_editor1 = make Marc_editor , :item_barcode => uniq_number  ,:title => uniq_alphanums
+  @marc_editor1.create_bib
+  @marc_editor1.create_holding
+  @marc_editor1.create_item
+
+  @marc_editor2 = make Marc_editor , :item_barcode => uniq_number  ,:title => uniq_alphanums
+  @marc_editor2.create_bib
+  @marc_editor2.create_holding
+  @marc_editor2.create_item
+
+  @edit = make Global_edit_dataobject , :item_title1 => @marc_editor1.title , :item_title2 => @marc_editor2.title
+  @edit.global_edit
+  @edit.verify_item_locations(@marc_editor1.title , 1)
+  @edit.verify_item_locations( @marc_editor2.title , 2)
+end
+
+Then(/^the changes should be successfully updated$/) do
+
+  @edit.location1.should == "API"
+  @edit.location2.should == "API"
+
+end
